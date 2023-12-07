d1fedd0973e0d3c5f3a95c7b4856c345782b9afa
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-07 16:17:29 +0900

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// App.test.tsx
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../src/components/App";
+import React from "react";
+
+describe("App Component", () => {
+  test("renders App component", () => {
+    render(<App />);
+
+    // Check if the component renders without crashing
+    expect(screen.getByText("メモアプリ")).toBeInTheDocument();
+  });
+
+  test("adds a memo", () => {
+    render(<App />);
+
+    // Simulate typing in the text field
+    userEvent.type(screen.getByLabelText("メモを追加"), "Test Memo");
+
+    // Click the 追加 button
+    fireEvent.click(screen.getByText("追加"));
+
+    // Check if the memo is added to the list
+    expect(screen.getByText("Test Memo")).toBeInTheDocument();
+  });
+
+  test("deletes a memo", () => {
+    render(<App />);
+
+    // Add a memo
+    userEvent.type(screen.getByLabelText("メモを追加"), "Test Memo");
+    fireEvent.click(screen.getByText("追加"));
+
+    // Check if the memo is added to the list
+    expect(screen.getByText("Test Memo")).toBeInTheDocument();
+
+    // Click the 削除 button
+    fireEvent.click(screen.getByText("削除"));
+
+    // Check if the memo is removed from the list
+    expect(screen.queryByText("Test Memo")).not.toBeInTheDocument();
+  });
 });
